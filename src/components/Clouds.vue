6b7e280b3feb1e33ae6d4881e5b986592c34ebce
Shaye Ulman <shayeulman@gmail.com>
2019-10-31 22:44:44 +0200

    added & approved clouds

--- src/components/Clouds.vue
+++ src/components/Clouds.vue
@@ -114,6 +114,25 @@
         d="M406.1 227.63c-8.23-103.65-144.71-137.8-200.49-49.05 -36.18-20.46-82.33 3.61-85.22 45.9C80.73 229.34 50 263.12 50 304.1c0 44.32 35.93 80.25 80.25 80.25h251.51c44.32 0 80.25-35.93 80.25-80.25C462 268.28 438.52 237.94 406.1 227.63z"
       />
     </svg>
+    <svg
+      class="clouds cloud7"
+      :style="{top: `${random()}px`}"
+      xmlns="http://www.w3.org/2000/svg"
+      xmlns:xlink="http://www.w3.org/1999/xlink"
+      version="1.1"
+      x="0"
+      y="0"
+      width="512"
+      height="512"
+      viewBox="0 0 512 512"
+      enable-background="new 0 0 512 512"
+      xml:space="preserve"
+    >
+      <path
+        id="cloud-icon"
+        d="M406.1 227.63c-8.23-103.65-144.71-137.8-200.49-49.05 -36.18-20.46-82.33 3.61-85.22 45.9C80.73 229.34 50 263.12 50 304.1c0 44.32 35.93 80.25 80.25 80.25h251.51c44.32 0 80.25-35.93 80.25-80.25C462 268.28 438.52 237.94 406.1 227.63z"
+      />
+    </svg>
   </div>
 </template>
 
@@ -121,7 +140,10 @@
 export default {
   methods: {
     random() {
-      return Math.floor(Math.random() * 200);
+      return (
+        Math.floor(Math.random() * (Math.floor(200) - Math.ceil(-100) + 1)) +
+        Math.ceil(-100)
+      );
     }
   }
 };
@@ -135,7 +157,7 @@ export default {
   height: 50vh;
   width: 100vh;
   background-size: 100% 100%;
-  z-index: -999;
+  //   z-index: -999;
 }
 
 .clouds {
@@ -145,6 +167,7 @@ export default {
 .cloud1 {
   width: 120px;
   height: 200px;
+  opacity: 0.5;
   z-index: 100;
   fill: var(--color-1-light);
   /* Animation shorthand syntax order: name, duration, timing-function, delay, iteration-count, direction, fill-mode */
@@ -169,23 +192,25 @@ export default {
 .cloud3 {
   width: 100px;
   height: 150px;
+  opacity: 0.5;
   z-index: 200;
   fill: var(--color-1-light);
-  -webkit-animation: move 35s linear 10s infinite backwards;
-  -moz-animation: move 35s linear 10s infinite backwards;
-  -o-animation: move 35s linear 10s infinite backwards;
-  animation: move 35s linear 10s infinite backwards;
+  -webkit-animation: move 35s linear 15s infinite backwards;
+  -moz-animation: move 35s linear 15s infinite backwards;
+  -o-animation: move 35s linear 15s infinite backwards;
+  animation: move 35s linear 15s infinite backwards;
 }
 
 .cloud4 {
   width: 180px;
   height: 260px;
+  opacity: 0.6q;
   z-index: 200;
   fill: var(--color-1-light);
-  -webkit-animation: move 25s linear 10s infinite backwards;
-  -moz-animation: move 25s linear 10s infinite backwards;
-  -o-animation: move 25s linear 10s infinite backwards;
-  animation: move 25s linear 10s infinite backwards;
+  -webkit-animation: move 25s linear 20s infinite backwards;
+  -moz-animation: move 25s linear 20s infinite backwards;
+  -o-animation: move 25s linear 20s infinite backwards;
+  animation: move 25s linear 20s infinite backwards;
 }
 
 .cloud5 {
@@ -194,10 +219,10 @@ export default {
   opacity: 0.3;
   z-index: 200;
   fill: var(--color-1-light);
-  -webkit-animation: move 18s linear 8s infinite backwards;
-  -moz-animation: move 18s linear 8s infinite backwards;
-  -o-animation: move 18s linear 8s infinite backwards;
-  animation: move 18s linear 8s infinite backwards;
+  -webkit-animation: move 18s linear 25s infinite backwards;
+  -moz-animation: move 18s linear 25s infinite backwards;
+  -o-animation: move 18s linear 25s infinite backwards;
+  animation: move 18s linear 25s infinite backwards;
 }
 
 .cloud6 {
@@ -205,10 +230,22 @@ export default {
   height: 90px;
   z-index: 200;
   fill: var(--color-1-light);
-  -webkit-animation: move 35s linear 10s infinite backwards;
-  -moz-animation: move 35s linear 10s infinite backwards;
-  -o-animation: move 35s linear 10s infinite backwards;
-  animation: move 35s linear 10s infinite backwards;
+  -webkit-animation: move 35s linear 30s infinite backwards;
+  -moz-animation: move 35s linear 30s infinite backwards;
+  -o-animation: move 35s linear 30s infinite backwards;
+  animation: move 35s linear 30s infinite backwards;
+}
+
+.cloud7 {
+  width: 280px;
+  height: 500px;
+  z-index: 200;
+  opacity: 0.3;
+  fill: var(--color-1-light);
+  -webkit-animation: move 35s linear 30s infinite backwards;
+  -moz-animation: move 35s linear 30s infinite backwards;
+  -o-animation: move 35s linear 30s infinite backwards;
+  animation: move 35s linear 30s infinite backwards;
 }
 
 @-webkit-keyframes move {
